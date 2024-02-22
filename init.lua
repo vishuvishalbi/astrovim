fab3c81fad03ad608f382f2785e6650ff6a3c9f1
vishuvishalbi <vishu_vishal_bi@live.com>
2024-02-22 19:08:43 +0530

    Update init.lua
    Default theme changed

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "onedark", --astrodark
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
